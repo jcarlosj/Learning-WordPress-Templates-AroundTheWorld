7d6f16baa36b308af9f06acf00499dcd61119189
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-11-27 13:54:15 -0500

    Implementa estilos para el título principal de las páginas (page.php)

--- wp-content/themes/around-the-world/page.php
+++ wp-content/themes/around-the-world/page.php
@@ -4,7 +4,7 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php the_title(); ?></h1>
+			<h1><span><?php the_title(); ?></span></h1>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
@@ -12,8 +12,6 @@
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
 
 				<br class="clear">
 
@@ -40,6 +38,6 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php # get_sidebar(); ?>
 
 <?php get_footer(); ?>
