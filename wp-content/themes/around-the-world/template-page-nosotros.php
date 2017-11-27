b6cd0982df7e0a4380ebf94225b7b22f6f896a77
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-11-27 17:18:04 -0500

    Agrega 'Grid' redistribuye el contenido de la página Nosotros

--- wp-content/themes/around-the-world/template-page-nosotros.php
+++ wp-content/themes/around-the-world/template-page-nosotros.php
@@ -8,16 +8,26 @@
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="clear">
 
 			<h1><span><?php the_title(); ?></span></h1>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<article id="post-<?php the_ID(); ?>" <?php post_class( 'grid_2-3' ); ?>>
 
 				<?php the_content(); ?>
+
+				<br class="clear">
+
+				<?php edit_post_link(); ?>
+
+			</article>
+			<!-- /article -->
+
+			<div class="gallery grid_1-3">
+
 				<?php # WordPress debe desplegar los ID de las imagenes
 				      # (ir al Admin y cambiar esta opcion para cada campo en el ACF)
 						$image = get_field( 'imagen_superior' );
@@ -39,12 +49,7 @@
 						}
 				?>
 
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
-
-			</article>
-			<!-- /article -->
+			</div>
 
 		<?php endwhile; ?>
 
