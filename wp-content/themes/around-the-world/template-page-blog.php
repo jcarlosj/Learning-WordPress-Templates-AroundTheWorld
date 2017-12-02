b97cf26d71cc8126ebabdd2ec0032b65bb94afe2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-02 14:47:51 -0500

    Implementa 'WP_Query' visualiza registros 'post' excepto el último (entradas del Blog) para la página Consejos

--- wp-content/themes/around-the-world/template-page-blog.php
+++ wp-content/themes/around-the-world/template-page-blog.php
@@ -31,15 +31,59 @@
 
 				<article class="last-entry clear">
 
-					<div class="edge-photography">
-						<?php the_post_thumbnail( 'main-image-blog' ); ?>
-					</div>
+					<a href="<?php the_permalink(); ?>">
+						<div class="edge-photography">
+							<?php the_post_thumbnail( 'main-image-blog' ); ?>
+						</div>
+					</a>
 
 					<div class="grid_1-3">
 
 					</div>
 					<div class="grid_2-3">
-						<h2><?php the_title(); ?></h2>
+						<h2>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h2>
+						<?php html5wp_excerpt('html5wp_custom_post'); ?>
+					</div>
+
+				</article>
+
+			<?php endwhile; wp_reset_postdata(); ?>
+
+			<?php
+			  # Imprime todos las entradas menos la última (por fecha) del Blog: Página Consejos
+				$args = array(
+					'order'          => 'DESC',		# Orden Ascendente
+					'orderby'        => 'date',	  # Ordenar por fecha
+					'post_type'      => 'post',	  # Nombre del Post Type: 'post' hace referencia a las entradas del Blog
+					'posts_per_page' => 5,				# Número de publicaciones por página
+					'offset'         => 1         # Número de 'Post' que se va a saltar
+				);
+
+				$consejos = new WP_Query( $args );
+
+				while( $consejos -> have_posts() ) :
+					$consejos -> the_post();
+			?>
+
+				<article class="entry-tip clear">
+
+					<div class="grid_1-3">
+						<a href="<?php the_permalink(); ?>">
+							<div class="edge-photography">
+								<?php the_post_thumbnail( 'image-medium' ); ?>
+							</div>
+						</a>
+					</div>
+					<div class="grid_2-3">
+						<h2>
+							<a href="<?php the_permalink(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h2>
 						<?php html5wp_excerpt('html5wp_custom_post'); ?>
 					</div>
 
