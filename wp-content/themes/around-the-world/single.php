6938e7c919a87363c81b787f6e6f4c421f8390e2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-06 14:50:21 -0500

    Agrega estilos 'Single Page' predeterminado (single.php)

--- wp-content/themes/around-the-world/single.php
+++ wp-content/themes/around-the-world/single.php
@@ -23,11 +23,11 @@
 
 			<div class="grid_1-3">
 				<!-- post details -->
-				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-				<!-- /post details -->
+				<span class="date">Escrito el: <?php the_time('F j, Y'); ?> </span>
+				<span class="author"><?php _e( 'Publicado por: ', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+				<span class="category"><?php _e( 'Categoría: ', 'html5blank' ); the_category(', '); // Separated by commas ?></span>
+				<!-- post details -->
 
-				<span><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></span>
 				<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
 				<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
 			</div>
