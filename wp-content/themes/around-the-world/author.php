ec7d52b79fbbe70c2aaa8db2dede8cc20ab5d1dd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-08 20:07:29 -0500

    Agrega estilos a la página predeterminada de autor (author.php)

--- wp-content/themes/around-the-world/author.php
+++ wp-content/themes/around-the-world/author.php
@@ -6,7 +6,11 @@
 
 		<?php if (have_posts()): the_post(); ?>
 
-			<h1><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h1>
+			<h1>
+				<span>
+					<?php _e( 'Todas las publicaciones de: ', 'html5blank' ); echo get_the_author(); ?>
+				</span>
+			</h1>
 
 		<?php if ( get_the_author_meta('description')) : ?>
 
@@ -21,15 +25,7 @@
 		<?php rewind_posts(); while (have_posts()) : the_post(); ?>
 
 			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<!-- post thumbnail -->
-				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-						<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-					</a>
-				<?php endif; ?>
-				<!-- /post thumbnail -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class( 'clear' ); ?>>
 
 				<!-- post title -->
 				<h2>
@@ -37,13 +33,28 @@
 				</h2>
 				<!-- /Post title -->
 
-				<!-- post details -->
-				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-				<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-				<!-- /post details -->
+				<div class="grid_1-3">
+					<!-- post thumbnail -->
+					<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+							<?php the_post_thumbnail( 'image-medium' ); // Declare pixel size you need inside the array ?>
+						</a>
+					<?php endif; ?>
+					<!-- /post thumbnail -->
+				</div>
+				<!-- .grid_1-3 -->
 
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+				<div class="grid_2-3">
+					<!-- post details -->
+					<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+					<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+					<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+					<!-- /post details -->
+
+					<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+
+				</div>
+				<!-- .grid_2-3 -->
 
 				<br class="clear">
 
@@ -72,6 +83,6 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+<?php # get_sidebar(); ?>
 
 <?php get_footer(); ?>
