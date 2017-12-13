27fb2c3509f0563b4c2e67a7838f3b59973feb26
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-12-13 11:36:06 -0500

    Despliega 'Slideshow' de 'Vegas 2 jQuery Plugin' como fondo en el Login

--- wp-content/themes/around-the-world/login/js/login.js
+++ wp-content/themes/around-the-world/login/js/login.js
@@ -1,3 +1,10 @@
 jQuery( function( $ ) {
-  console .log( login_images . template_route );
+  /* Implementa 'Vegas jQuery Plugin' en el Login */
+  $( 'body' ) .vegas({
+    slides: [   /* Paths de las imagenes */
+      { src: login_images . template_route + '/login/img/1.jpg' },
+      { src: login_images . template_route + '/login/img/2.jpg' },
+      { src: login_images . template_route + '/login/img/3.jpg' }
+    ]
+  });
 });
